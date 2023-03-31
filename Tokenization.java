419c438ef85cf950ca803f9e8810b248899907bc
Blessing Onwunali <76081468+Chifrica@users.noreply.github.com>
2023-04-01 00:25:21 +0100

    Update Tokenization.java
    This is my second commit.

--- Tokenization.java
+++ Tokenization.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 public class Tokenization{
     public static void main(String[] args) {
+        
+//         Write a JAVA program that reads a sentence (20 words with four punctuation marks). 
+//           Break the sentence into individual tokens (delimiters should also be regarded as token) and PRINT the output.		
+//           [NOTE: Use the Scanner class]
+            
         //Declaring a Scanner
         Scanner input = new Scanner(System.in);
         //Creating a String
